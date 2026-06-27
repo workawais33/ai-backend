91003f4af1a2e70f2208677c771f4ed67782006a
Awais Shahzad <workawais9@gmail.com>
2026-06-27 13:57:19 +0500

    Prepare NestJS for Railway deployment

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors({
-    origin: process.env.FRONTEND_URL,
-    credentials: true,
-  });
+ app.enableCors({
+  origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+  credentials: true,
+});
 
   await app.listen(process.env.PORT || 5000);
 }
